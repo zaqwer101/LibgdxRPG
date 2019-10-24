fa9b14c1b8bcd3161bca2fe75edface451bb12f4
zaqwer101 <platonov256@gmail.com>
2019-10-25 01:55:18 +0800

    Testing: Обработчик атаки и несколько предметов

--- core/src/tech/yoba/rpg/LibgdxRPG.java
+++ core/src/tech/yoba/rpg/LibgdxRPG.java
@@ -1,11 +1,13 @@
 package tech.yoba.rpg;
 
+import Game.Actions.AttackAction;
 import Game.Actions.MoveAction;
 import Game.Attacks.MeleeAttack;
 import Game.Core.Creature;
 import Game.Core.Location;
 import Game.Core.Position;
 import Game.Core.Stats;
+import Game.Items.Item;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
@@ -32,7 +34,7 @@ public class LibgdxRPG extends ApplicationAdapter {
 	public void create () {
 		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("OpenSans-Regular.ttf"));
 		FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-		parameter.size = 10;
+		parameter.size = 12;
 		font = generator.generateFont(parameter); // font size 12 pixels
 		generator.dispose();
 		font.setColor(Color.BLACK);
@@ -42,9 +44,13 @@ public class LibgdxRPG extends ApplicationAdapter {
 		actorStats.setStat("endurance", 6);
 		location = new Location(30, 30);
 		actor = new Creature("Actor", '@', new Position(2, 2),location);
+		Item item = new Item("Kek", 1, 1);
+		location.getLocation().get(100).addItem(item);
 		actor.addAttack(new MeleeAttack(2));
 		actor.addStats(actorStats);
+		actor.addAttack(new MeleeAttack(1));
 		enemy = new Creature("Enemy", 'E', new Position(10, 10),location);
+		enemy.addToInventory(item);
 	}
 
 	public void drawMap(ShapeRenderer batch, Location location)
@@ -67,6 +73,13 @@ public class LibgdxRPG extends ApplicationAdapter {
 					isFilled = true;
 					spriteColor = Color.RED;
 					break;
+				case '\"':
+					isFilled = false;
+					SpriteBatch fontBatch = new SpriteBatch();
+					fontBatch.begin();
+					font.draw(fontBatch, "\"", l.getX() * size + 8, l.getY() * size + 8);
+					fontBatch.end();
+					break;
 			}
 
 			batch.setColor(spriteColor);
@@ -86,29 +99,19 @@ public class LibgdxRPG extends ApplicationAdapter {
 
 	public void keyboardHandler()
 	{
-		if (Gdx.input.isKeyJustPressed(Input.Keys.UP))
+		if (Gdx.input.isKeyPressed(Input.Keys.A))
 		{
-			actor.performAction(new MoveAction(actor, new int[] {0, 1} ));
+			if (Gdx.input.isKeyJustPressed(Input.Keys.UP))    actor.performAction(new AttackAction(actor, actor.getLocation().getPosition(actor.getPosition().getX(), actor.getPosition().getY() + 1).getMember(), 1, actor.getAttacks()[0]));
+			if (Gdx.input.isKeyJustPressed(Input.Keys.DOWN))  actor.performAction(new AttackAction(actor, actor.getLocation().getPosition(actor.getPosition().getX(), actor.getPosition().getY() - 1).getMember(), 1, actor.getAttacks()[0]));
+			if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT))  actor.performAction(new AttackAction(actor, actor.getLocation().getPosition(actor.getPosition().getX() - 1, actor.getPosition().getY()).getMember(), 1, actor.getAttacks()[0]));
+			if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) actor.performAction(new AttackAction(actor, actor.getLocation().getPosition(actor.getPosition().getX() + 1, actor.getPosition().getY()).getMember(), 1, actor.getAttacks()[0]));
+			return;
 		}
-		if (Gdx.input.isKeyJustPressed(Input.Keys.DOWN))
-		{
-			actor.performAction(new MoveAction(actor, new int[] {0, -1} ));
-		}
-
-		if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT))
-		{
-			actor.performAction(new MoveAction(actor, new int[] {-1, 0} ));
-		}
-
-		if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT))
-		{
-			actor.performAction(new MoveAction(actor, new int[] {1, 0} ));
-		}
-
-		if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE))
-		{
-			actor.endTurn();
-		}
+		if (Gdx.input.isKeyJustPressed(Input.Keys.UP))    { actor.performAction(new MoveAction(actor, new int[] {0, 1} )); 	}
+		if (Gdx.input.isKeyJustPressed(Input.Keys.DOWN))  { actor.performAction(new MoveAction(actor, new int[] {0, -1} )); }
+		if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT))  { actor.performAction(new MoveAction(actor, new int[] {-1, 0} )); }
+		if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) { actor.performAction(new MoveAction(actor, new int[] {1, 0} )); 	}
+		if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) { actor.endTurn(); enemy.endTurn(); }
 	}
 
 	public void drawUI()
